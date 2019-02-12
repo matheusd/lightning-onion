1a50057d31be22009699c57ce18c1f2d87215563
Christian Decker <decker.christian@gmail.com>
2019-02-12 12:14:37 +0100

    tool: Add an onion spec in JSON that can be passed in
    This should make testing a lot easier, since we can just have a bunch of these
    sitting in the repo and test them.
    
    Signed-off-by: Christian Decker <decker.christian@gmail.com>

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"encoding/hex"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -14,6 +15,61 @@ import (
 	sphinx "github.com/decred/lightning-onion"
 )
 
+type OnionHopSpec struct {
+	Realm     int    `json:"realm"`
+	PublicKey string `json:"pubkey"`
+	Payload   string `json:"payload"`
+}
+
+type OnionSpec struct {
+	SessionKey string         `json:"session_key,omitempty"`
+	Hops       []OnionHopSpec `json:"hops"`
+}
+
+func parseOnionSpec(spec OnionSpec) (*sphinx.PaymentPath, *secp256k1.PrivateKey, error) {
+	var path sphinx.PaymentPath
+	var binSessionKey []byte
+	var err error
+
+	if spec.SessionKey != "" {
+		binSessionKey, err = hex.DecodeString(spec.SessionKey)
+		if err != nil {
+			log.Fatalf("Unable to decode the sessionKey %v: %v\n", spec.SessionKey, err)
+		}
+
+		if len(binSessionKey) != 32 {
+			log.Fatalf("Session key must be a 32 byte hex string: %v\n", spec.SessionKey)
+		}
+	} else {
+		binSessionKey = bytes.Repeat([]byte{'A'}, 32)
+	}
+
+	sessionKey, _ := secp256k1.PrivKeyFromBytes(binSessionKey)
+
+	for i, hop := range spec.Hops {
+		binKey, err := hex.DecodeString(hop.PublicKey)
+		if err != nil || len(binKey) != 33 {
+			log.Fatalf("%s is not a valid hex pubkey %s", hop.PublicKey, err)
+		}
+
+		pubkey, err := secp256k1.ParsePubKey(binKey)
+		if err != nil {
+			log.Fatalf("%s is not a valid hex pubkey %s", hop.PublicKey, err)
+		}
+
+		path[i].NodePub = *pubkey
+
+		path[i].HopPayload.Realm[0] = byte(hop.Realm)
+		path[i].HopPayload.Payload, err = hex.DecodeString(hop.Payload)
+		if err != nil {
+			log.Fatalf("%s is not a valid hex payload %s", hop.Payload, err)
+		}
+
+		fmt.Fprintf(os.Stderr, "Node %d pubkey %x\n", i, pubkey.SerializeCompressed())
+	}
+	return &path, sessionKey, nil
+}
+
 // main implements a simple command line utility that can be used in order to
 // either generate a fresh mix-header or decode and fully process an existing
 // one given a private key.
@@ -22,48 +78,36 @@ func main() {
 
 	assocData := bytes.Repeat([]byte{'B'}, 32)
 
-	if len(args) < 2 {
-		fmt.Printf("Usage: %s (generate|decode) <private-keys>\n", args[0])
+	if len(args) < 3 {
+		fmt.Printf("Usage: %s (generate|decode) <input-file>\n", args[0])
 		os.Exit(1)
 	}
 
 	switch args[1] {
 	case "generate":
-		var path sphinx.PaymentPath
-		for i, hexKey := range args[2:] {
-			binKey, err := hex.DecodeString(hexKey)
-			if err != nil || len(binKey) != 33 {
-				log.Fatalf("%s is not a valid hex pubkey %s", hexKey, err)
-			}
+		var spec OnionSpec
 
-			pubkey, err := secp256k1.ParsePubKey(binKey)
-			if err != nil {
-				panic(err)
-			}
-
-			path[i] = sphinx.OnionHop{
-				NodePub: *pubkey,
-				HopData: sphinx.HopData{
-					Realm:         [1]byte{0x00},
-					ForwardAmount: uint64(i),
-					OutgoingCltv:  uint32(i),
-				},
-			}
-			copy(path[i].HopData.NextAddress[:], bytes.Repeat([]byte{byte(i)}, 8))
-
-			fmt.Fprintf(os.Stderr, "Node %d pubkey %x\n", i, pubkey.SerializeCompressed())
+		jsonSpec, err := ioutil.ReadFile(args[2])
+		if err != nil {
+			log.Fatalf("Unable to read JSON onion spec from file %v: %v", args[2], err)
 		}
 
-		sessionKey, _ := secp256k1.PrivKeyFromBytes(bytes.Repeat([]byte{'A'}, 32))
+		if err := json.Unmarshal(jsonSpec, &spec); err != nil {
+			log.Fatalf("Unable to parse JSON onion spec: %v", err)
+		}
 
-		msg, err := sphinx.NewOnionPacket(&path, sessionKey, assocData)
+		path, sessionKey, err := parseOnionSpec(spec)
+		if err != nil {
+			log.Fatalf("could not parse onion spec: %v", err)
+		}
+
+		msg, err := sphinx.NewOnionPacket(path, sessionKey, assocData)
 		if err != nil {
 			log.Fatalf("Error creating message: %v", err)
 		}
 
 		w := bytes.NewBuffer([]byte{})
 		err = msg.Encode(w)
-
 		if err != nil {
 			log.Fatalf("Error serializing message: %v", err)
 		}
@@ -82,8 +126,11 @@ func main() {
 		}
 
 		privkey, _ := secp256k1.PrivKeyFromBytes(binKey)
-		s := sphinx.NewRouter(privkey, &chaincfg.TestNet3Params,
-			sphinx.NewMemoryReplayLog())
+		replay_log := sphinx.NewMemoryReplayLog()
+		s := sphinx.NewRouter(privkey, &chaincfg.TestNet3Params, replay_log)
+
+		replay_log.Start()
+		defer replay_log.Stop()
 
 		var packet sphinx.OnionPacket
 		err = packet.Decode(bytes.NewBuffer(binMsg))
